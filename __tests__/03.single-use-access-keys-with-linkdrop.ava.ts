55a10b39bbc871f88c1dc193125b0c4d40697464
Willem Wyndham <wyndham@cs.unc.edu>
2021-12-15 17:40:40 +0100

    fix: fmt

--- __tests__/03.single-use-access-keys-with-linkdrop.ava.ts
+++ __tests__/03.single-use-access-keys-with-linkdrop.ava.ts
@@ -11,7 +11,7 @@
  *
  * You can see this functionality in action below using `signWithKey`.
  */
-import {Workspace, createKeyPair, Gas, NEAR} from 'near-willem-workspaces-ava';
+import {Workspace, createKeyPair, NEAR} from 'near-willem-workspaces-ava';
 
 /* Contract API for reference
 impl Linkdrop {
